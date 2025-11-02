68e5d899436bf08cb873bc643bcf46ef79154cb9
Tom Shea <tom@tomshea.dev>
2025-11-02 08:18:08 -0800

    update package.json and add basic index file

---
+++ index.mjs
@@ -0,0 +1,11 @@
+import path from 'node:path';
+
+export function getInklecateBinaryPath() {
+    if ( process.platform === 'win32' ) {
+        return path.resolve(import.meta.dirname, 'bin', 'windows', 'inklecate.exe');
+    }
+    if ( process.platform === 'darwin' ) {
+        return path.resolve(import.meta.dirname, 'bin', 'mac', 'inklecate');
+    }
+    return path.resolve(import.meta.dirname, 'bin', 'linux', 'inklecate');
+}
